bf84d84b2589b44a8d4f03f5e888698f98a11e04
KiwiKetil <ketilsveberg@gmail.com>
2024-05-31 12:07:59 +0200

    First Commit

--- Check List API/Check List API/Entities/CheckList.cs
+++ Check List API/Check List API/Entities/CheckList.cs
@@ -1,5 +1,11 @@
 ﻿namespace Check_List_API.Entities;
 
+public readonly record struct ChecklistId(Guid checklistId)
+{
+    public static ChecklistId NewId => new (Guid.NewGuid());
+    public static ChecklistId Empty => new (Guid.Empty);
+};
+
 public class CheckList
 {
 
