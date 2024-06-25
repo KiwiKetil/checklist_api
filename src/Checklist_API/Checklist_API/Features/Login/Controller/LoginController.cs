8797ce3d02c7e5c7a55260c748d91a961c2286e0
Quyen Ho <quyen99@hotmail.com>
2024-06-25 09:58:16 +0200

    adding controller

---
+++ src/Checklist_API/Checklist_API/Features/Login/Controller/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using Check_List_API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Checklist_API.Features.Login.Controller;
+[Route("api/v1/login")]
+[ApiController]
+public class LoginController : ControllerBase
+{
+    private IConfiguration _config;
+    private readonly CheckListDbContext _DbContext;
+    private readonly ILogger<LoginController> _logger;
+    public LoginController()
+    {
+        
+    }
+}
