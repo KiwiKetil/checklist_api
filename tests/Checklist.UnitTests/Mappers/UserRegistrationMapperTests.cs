03f061da6a9503e41da45709b4534b11febfde31
Quyen Ho <quyen99@hotmail.com>
2024-09-25 11:19:36 +0200

    finish UserRegistrationMapperTests

--- tests/Checklist.UnitTests/Mappers/UserRegistrationMapperTests.cs
+++ tests/Checklist.UnitTests/Mappers/UserRegistrationMapperTests.cs
@@ -12,4 +12,34 @@ namespace Checklist.UnitTests.Mappers;
 public class UserRegistrationMapperTests
 {
     private readonly IMapper<User, UserRegistrationDTO> _userRegistrationMapper = new UserRegistrationMapper();
+
+    [Fact]
+    public void MapToEntity_WhenMappingRegistrationDTOToUser_ShouldReturnUser()
+    {
+        // Arrange
+
+        UserRegistrationDTO userRegistrationDTO = new
+            (
+            "Ketil",
+            "Sveberg",
+            "11112222",
+            "Ketilsveberg@gmail.com",
+            "123456"
+            );
+
+        // Act
+
+        var user = _userRegistrationMapper.MapToEntity( userRegistrationDTO );
+
+        // Assert
+
+        Assert.NotNull( user );
+        Assert.Equal(user.FirstName, userRegistrationDTO.FirstName);
+        Assert.Equal(user.LastName, userRegistrationDTO.LastName);
+        Assert.Equal(user.PhoneNumber, userRegistrationDTO.PhoneNumber);
+        Assert.Equal(user.Email, userRegistrationDTO.Email);
+
+        Assert.True(user.DateCreated > DateTime.Now.AddSeconds(-1) && user.DateCreated <= DateTime.Now, "DateCreated is not set correctly");
+        Assert.True(user.DateUpdated > DateTime.Now.AddSeconds(-1) && user.DateUpdated <= DateTime.Now, "DateUpdated is not set correctly");
+    }
 }
