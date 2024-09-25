49265af68692558c753f9a6c74d08d74a1d4649e
KiwiKetil <ketilsveberg@gmail.com>
2024-09-25 09:47:27 +0200

    finished UserMappertests

--- tests/Checklist.UnitTests/Mappers/UserMapperTests.cs
+++ tests/Checklist.UnitTests/Mappers/UserMapperTests.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Checklist_API.Features.Common.Interfaces;
+using Checklist_API.Features.Users.DTOs;
+using Checklist_API.Features.Users.Entity;
+using Checklist_API.Features.Users.Mappers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,4 +12,64 @@ namespace Checklist.UnitTests.Mappers;
 
 public class UserMapperTests
 {
+    private readonly UserMapper _userMapper = new();
+
+    [Fact]
+    public void MapToDTO_WhenMappingUserToUserDTO_ShouldReturnUserDTO() 
+    {
+        // Arrange
+
+        User user = new()
+        {
+            Id = UserId.NewId,
+            FirstName = "Ketil",
+            LastName = "Sveberg",
+            PhoneNumber = "11112222",
+            Email = "ketilsveberg@gmail.com",
+            HashedPassword = "$2a$11$6LeztI8J.RjEFt62ctA8a.yyM1V6X3bOEe6Rv5we2iPIe9namsjU6",
+            Salt = "$2a$11$9p6QfwD5gdrrg4Mvzf/Mgu",
+            DateCreated = new DateTime(2023, 12, 13, 11, 00, 00),
+            DateUpdated = new DateTime(2023, 12, 13, 11, 00, 00),
+        };
+
+        // Act
+
+        var userDTO = _userMapper.MapToDTO(user);
+
+        // Assert
+
+        Assert.NotNull(userDTO);
+        Assert.Equal(user.FirstName, userDTO.FirstName);
+        Assert.Equal(user.LastName, userDTO.LastName);
+        Assert.Equal(user.PhoneNumber, userDTO.PhoneNumber);
+        Assert.Equal(user.Email, userDTO.Email);
+        Assert.Equal(user.DateCreated, userDTO.DateCreated);
+        Assert.Equal(user.DateUpdated, userDTO.DateUpdated);    
+    }
+
+    [Fact]
+    public void MapToUser_WhenMappingUserDTOToUser_ShouldReturnUser()
+    {
+        // Arrange
+
+        UserDTO userDTO = new
+            ("Ketil",
+            "Sveberg",
+            "11112222",
+            "ketilsveberg@gmail.com",
+            new DateTime(2023, 12, 13, 11, 00, 00),
+            new DateTime(2023, 12, 13, 11, 00, 00));      
+
+        // Act
+
+        var user = _userMapper.MapToEntity(userDTO);
+
+        // Assert
+
+        Assert.NotNull(user);
+        Assert.Equal(user.FirstName, userDTO.FirstName);
+        Assert.Equal(user.LastName, userDTO.LastName);
+        Assert.Equal(user.PhoneNumber, userDTO.PhoneNumber);
+        Assert.Equal(user.Email, userDTO.Email);
+    }
 }
---
+++ tests/Checklist.UnitTests/Mappers/UserRegistrationMapperTests.cs
@@ -0,0 +1,15 @@
+﻿using Checklist_API.Features.Common.Interfaces;
+using Checklist_API.Features.Users.DTOs;
+using Checklist_API.Features.Users.Entity;
+using Checklist_API.Features.Users.Mappers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checklist.UnitTests.Mappers;
+public class UserRegistrationMapperTests
+{
+    private readonly IMapper<User, UserRegistrationDTO> _userRegistrationMapper = new UserRegistrationMapper();
+}
