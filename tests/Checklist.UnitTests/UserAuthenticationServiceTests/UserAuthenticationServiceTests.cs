38007846c1de61b8ad0b16a2daf1f3674642f986
KiwiKetil <ketilsveberg@gmail.com>
2024-10-21 13:46:03 +0200

    finished test AuthenticateUserAsync_WhenUserIsAuthenticated_ShouldReturnUser()

--- tests/Checklist.UnitTests/UserAuthenticationServiceTests/UserAuthenticationServiceTests.cs
+++ tests/Checklist.UnitTests/UserAuthenticationServiceTests/UserAuthenticationServiceTests.cs
@@ -1,73 +1,82 @@
-﻿//using Check_List_API.Data;
-//using Checklist_API.Features.JWT.Features;
-//using Checklist_API.Features.Login.DTOs;
-//using Checklist_API.Features.Users.Entity;
-//using Checklist_API.Features.Users.Repository.Interfaces;
-//using Microsoft.EntityFrameworkCore;
-//using Microsoft.Extensions.Logging;
-//using Moq;
-//using Serilog;
-//using System.Reflection.Metadata;
-//using System.Runtime.Intrinsics.X86;
+﻿using Check_List_API.Data;
+using Checklist_API.Features.JWT.Features;
+using Checklist_API.Features.JWT.Repository.Interfaces;
+using Checklist_API.Features.Login.DTOs;
+using Checklist_API.Features.Users.Entity;
+using Checklist_API.Features.Users.Repository;
+using Checklist_API.Features.Users.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Moq;
 
-//namespace Checklist.UnitTests.AuthenticationServiceTests;
+namespace Checklist.UnitTests.AuthenticationServiceTests;
 
-//public class UserAuthenticationServiceTests
-//{
-//    private readonly UserAuthenticationService _userAuthenticationService;
-//    private readonly CheckListDbContext _dbContext;
-//    private readonly Mock<ILogger<UserAuthenticationService>> _loggerMock = new();
+public class UserAuthenticationServiceTests
+{
+    private readonly UserAuthenticationService _userAuthenticationService;
+    private readonly Mock<IUserRepository> _userRepositoryMock = new();
+    private readonly Mock<ILogger<UserAuthenticationService>> _loggerMock = new();
 
-//    public UserAuthenticationServiceTests()
-//    {
-//        var options = new DbContextOptionsBuilder<CheckListDbContext>()
-//            .UseInMemoryDatabase(databaseName: "TestDatabase")
-//            .Options;
+    public UserAuthenticationServiceTests()
+    {      
+        _userAuthenticationService = new UserAuthenticationService(_userRepositoryMock.Object, _loggerMock.Object);
+    }
 
-//        _dbContext = new CheckListDbContext(options);
-//        _userAuthenticationService = new UserAuthenticationService(_dbContext, _loggerMock.Object);
-//    }
+    [Fact]
+    public async Task AuthenticateUserAsync_WhenUserIsAuthenticated_ShouldReturnUser()
+    {
+        // Arrange
 
-//    [Fact]
-//    public async Task AuthenticateUserAsync_WhenUserIsAuthenticated_ShouldReturnUser()
-//    {
-//        // Arrange
-//        var loginDTO = new LoginDTO { UserName = "ketilsveberg@gmail.com", Password = "string" };
-//        var expectedUser = new User
-//        {
-//            Id = UserId.NewId,
-//            FirstName = "Ketil",
-//            LastName = "Sveberg",
-//            Email = "ketilsveberg@gmail.com",
-//            HashedPassword = "$2a$11$J/m/v5v3hOVLKREX7jMZNO1xkMbtzU3vHf3Tm0Swc2MTszc0IpxO2",
-//            Salt = "$2a$11$55pfCgY8voiC1V4029QfR."
+        var loginDTO = new LoginDTO { UserName = "ketilsveberg@gmail.com", Password = "string" };
 
-//        };
+        var expectedUser = new User
+        {
+            Id = UserId.NewId,
+            FirstName = "Ketil",
+            LastName = "Sveberg",
+            Email = "ketilsveberg@gmail.com",
+            HashedPassword = "$2a$11$J/m/v5v3hOVLKREX7jMZNO1xkMbtzU3vHf3Tm0Swc2MTszc0IpxO2",
+            Salt = "$2a$11$55pfCgY8voiC1V4029QfR."
+        };       
 
-//        // Add the user to the in-memory database
-//        _dbContext.User.Add(expectedUser);
-//        _dbContext.SaveChanges();
+        _userRepositoryMock.Setup(x => x.GetByEmailAsync(loginDTO.UserName)).ReturnsAsync(expectedUser);
 
-//        // Act
-//        var result = await _userAuthenticationService.AuthenticateUserAsync(loginDTO);
+        // Act
 
-//        // Assert
-//        Assert.NotNull(result);
-//        Assert.Equal(expectedUser.Id, result.Id);
-//        Assert.Equal(expectedUser.FirstName, result.FirstName);
-//        Assert.Equal(expectedUser.LastName, result.LastName);
-//        Assert.Equal(expectedUser.Email, result.Email);
-//    }
+        var result = await _userAuthenticationService.AuthenticateUserAsync(loginDTO);
 
-//    [Fact]
-//    public async Task AuthenticateUserAsync_WhenUserIsNotAuthenticated_ShouldReturnNull()
-//    {
-//        // Arrange
+        // Assert
+
+        Assert.NotNull(result);
+        Assert.Equal(expectedUser.Id, result.Id);
+        Assert.Equal(expectedUser.FirstName, result.FirstName);
+        Assert.Equal(expectedUser.LastName, result.LastName);
+        Assert.Equal(expectedUser.Email, result.Email);
+
+        _userRepositoryMock.Verify(x => x.GetByEmailAsync(loginDTO.UserName), Times.Once);
+    }
+
+    [Fact]
+    public async Task AuthenticateUserAsync_WhenUserIsNotAuthenticated_ShouldReturnNull()
+    {
+        // Arrange
 
 
-//        // Act
+        // Act
 
 
-//        // Assert
-//    }
-//}
+        // Assert
+    }
+
+    [Fact]
+    public async Task AuthenticateUserAsync_WhenUserDoesNotExist_ShouldReturnNull()
+    {
+        // Arrange
+
+
+        // Act
+
+
+        // Assert
+    }
+}
