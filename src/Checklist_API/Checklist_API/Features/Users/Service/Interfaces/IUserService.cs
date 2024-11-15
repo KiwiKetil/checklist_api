1476eaf4193fb788dce7528f47d726d41eb849ab
KiwiKetil <ketilsveberg@gmail.com>
2024-11-15 12:44:38 +0100

    finishded GetUserByID()

--- src/Checklist_API/Checklist_API/Features/Users/Service/Interfaces/IUserService.cs
+++ src/Checklist_API/Checklist_API/Features/Users/Service/Interfaces/IUserService.cs
@@ -5,7 +5,7 @@ namespace Checklist_API.Features.Users.Service.Interfaces;
 public interface IUserService
 {
     Task<IEnumerable<UserDTO>> GetAllAsync(int page, int pageSize);
-    Task<UserDTO?> GetByIdAsync(Guid UserId);
+    Task<UserDTO?> GetByIdAsync(Guid id);
     Task<UserDTO?> UpdateAsync(Guid id, UserDTO dto);
     Task<UserDTO?> DeleteAsync(Guid id);
     Task<UserDTO?> RegisterUserAsync(UserRegistrationDTO dto);
